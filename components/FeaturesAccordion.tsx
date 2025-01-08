7e92216cc51c99d4c64c1dacbca636fabefd9893
Brendan McLaughlin <brendanmclaughlin@Brendans-MacBook-Pro.local>
2025-01-08 23:02:24 +1100

    feat: copy for landing page v1

--- components/FeaturesAccordion.tsx
+++ components/FeaturesAccordion.tsx
@@ -216,10 +216,10 @@ const FeaturesAccordion = () => {
     >
       <div className="px-8">
         <h2 className="font-extrabold text-4xl lg:text-6xl tracking-tight mb-12 md:mb-24">
-          All you need to ship your startup fast
-          <span className="bg-neutral text-neutral-content px-2 md:px-4 ml-1 md:ml-1.5 leading-relaxed">
-            and get profitable
-          </span>
+          Type Desrciption, Get Code
+          {/* <span className="inline-block bg-neutral text-neutral-content px-2 md:px-4 ml-1 md:ml-1.5 mt-4">
+            Get HTS code
+          </span> */}
         </h2>
         <div className=" flex flex-col md:flex-row gap-12 md:gap-24">
           <div className="grid grid-cols-1 items-stretch gap-8 sm:gap-12 lg:grid-cols-2 lg:gap-20">
