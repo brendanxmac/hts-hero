c5537a69af5f9b6c3d25c6ba5baa1895bd69d5f5
Brendan McLaughlin <brendanmclaughlin@Brendans-MacBook-Pro.local>
2025-01-18 14:53:15 +1100

    fix: results-ui

--- components/ProductDescriptionHeader.tsx
+++ components/ProductDescriptionHeader.tsx
@@ -8,9 +8,7 @@ export const ProductDescriptionHeader = ({ description }: Props) => {
   return (
     <div className="w-full max-w-4xl flex flex-col gap-2 rounded-md">
       <SectionLabel value="Product Description" />
-      <h3 className="font-bold text-white text-2xl lg:text-3xl ">
-        {description}
-      </h3>
+      <h3 className="font-bold text-white lg:text-xl ">{description}</h3>
     </div>
   );
 };
