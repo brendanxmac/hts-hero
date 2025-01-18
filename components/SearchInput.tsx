df4bc47e469d849f130f0ddd19fea1cb5404ec79
Brendan McLaughlin <brendanmclaughlin@Brendans-MacBook-Pro.local>
2025-01-18 14:53:15 +1100

    fix: results-ui

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -13,7 +13,7 @@ export default function SearchInput({
   placeholder,
   setProductDescription,
 }: Props) {
-  const characterLimit = 250;
+  const characterLimit = 400;
   const [localProductDescription, setLocalProductDescription] = useState("");
   const textareaRef = useRef<HTMLTextAreaElement | null>(null);
 
@@ -50,10 +50,10 @@ export default function SearchInput({
   };
 
   return (
-    <div className="w-full max-w-2xl flex flex-col gap-2 bg-black py-3">
+    <div className="w-full max-w-3xl flex flex-col gap-2 bg-black py-4">
       <div
         className={
-          "w-full flex flex-col gap-3 bg-neutral-900 items-start rounded-2xl p-4 mx-auto"
+          "w-full flex flex-col gap-2 bg-neutral-900 items-start rounded-2xl px-4 py-3 mx-auto"
         }
       >
         <div className="w-full flex">
@@ -65,9 +65,10 @@ export default function SearchInput({
             onChange={handleInputChange}
             // @ts-ignore
             onKeyDown={handleKeyDown}
-            className="flex-grow resize-none bg-inherit text-neutral-300 placeholder-neutral-600 focus:ring-0 focus:outline-none"
+            className="flex-grow resize-none bg-inherit text-neutral-300 placeholder-neutral-600 focus:ring-0 focus:outline-none text-sm"
           ></textarea>
         </div>
+
         <div className="w-full flex justify-between items-center">
           <p
             className={classNames(
@@ -94,9 +95,7 @@ export default function SearchInput({
             disabled={localProductDescription.length > characterLimit}
             className={classNames(
               localProductDescription ? "bg-white" : "bg-neutral-700",
-              localProductDescription.length > characterLimit &&
-                "bg-neutral-700",
-              "h-7 w-7 rounded-lg  flex items-center justify-center text-sm font-bold"
+              "h-7 w-7 rounded-lg flex items-center justify-center text-sm font-bold disabled:bg-neutral-600"
             )}
           >
             <ArrowUpIcon
