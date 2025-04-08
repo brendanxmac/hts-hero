21e10313005b0a86da4f0e74d9359bed5d51b7df
Brendan McLaughlin <brendanmclaughlin@Brendans-MacBook-Pro.local>
2025-04-09 06:33:57 +1000

    feat: map special tariff codes to general notes

--- public/notes/notes.tsx
+++ public/notes/notes.tsx
@@ -2,6 +2,7 @@ export interface Note {
   title: string;
   description: string;
   pdfURL: string;
+  specialTariffTreatmentCodes?: string[];
 }
 
 export const notes: Note[] = [
@@ -9,163 +10,196 @@ export const notes: Note[] = [
     title: "General Rules of Interpretation",
     description: "General Rules of Interpretation",
     pdfURL: "/notes/general/General Rules of Interpretation.pdf",
+    specialTariffTreatmentCodes: [],
   },
   {
     title: "General Note 1",
     description:
       "Tariff Treatment of Imported Goods and of Vessel Equipments, Parts and Repairs",
     pdfURL: "/notes/general/General Note 1.pdf",
+    specialTariffTreatmentCodes: [],
   },
   {
     title: "General Note 2",
     description: "Customs Territory of the United States",
     pdfURL: "/notes/general/General Note 2.pdf",
+    specialTariffTreatmentCodes: [],
   },
   {
     title: "General Note 3",
     description: "Rates of Duty",
     pdfURL: "/notes/general/General Note 3.pdf",
+    specialTariffTreatmentCodes: [],
   },
   {
     title: "General Note 4",
     description: "Generalized System of Preferences (GSP)",
     pdfURL: "/notes/general/General Note 4.pdf",
+    specialTariffTreatmentCodes: ["A", "A*", "A+"],
   },
   {
     title: "General Note 5",
     description: "Automotive Products Trade Act",
     pdfURL: "/notes/general/General Note 5.pdf",
+    specialTariffTreatmentCodes: ["B"],
   },
   {
     title: "General Note 6",
     description: "Agreement on Trade in Civil Aircraft",
     pdfURL: "/notes/general/General Note 6.pdf",
+    specialTariffTreatmentCodes: ["C"],
   },
   {
     title: "General Note 7",
     description: "Caribbean Basin Economic Recovery Act (CBERA)",
     pdfURL: "/notes/general/General Note 7.pdf",
+    specialTariffTreatmentCodes: ["E", "E*"],
   },
   {
     title: "General Note 8",
     description:
       "United States-Israel Free Trade Area Implementation Act of 1985",
     pdfURL: "/notes/general/General Note 8.pdf",
+    specialTariffTreatmentCodes: ["IL"],
   },
   {
     title: "General Note 9",
     description: "United States-Canada Free-Trade Agreement-Suspended",
     pdfURL: "/notes/general/General Note 9.pdf",
+    specialTariffTreatmentCodes: [],
   },
   {
     title: "General Note 10",
     description: "Products of the Freely Associated States",
     pdfURL: "/notes/general/General Note 10.pdf",
+    specialTariffTreatmentCodes: [],
   },
   {
     title: "General Note 11",
     description: "United States-Mexico-Canada Agreement",
     pdfURL: "/notes/general/General Note 11.pdf",
+    specialTariffTreatmentCodes: [],
   },
   {
     title: "General Note 12",
     description: "North American Free Trade Agreement-Deleted",
     pdfURL: "/notes/general/General Note 12.pdf",
+    specialTariffTreatmentCodes: [],
   },
   {
     title: "General Note 13",
     description: "Pharmaceutical products and related appendix",
     pdfURL: "/notes/general/General Note 13.pdf",
+    specialTariffTreatmentCodes: ["K"],
   },
   {
     title: "General Note 14",
     description: "Intermediate chemicals for dyes and related appendix",
     pdfURL: "/notes/general/General Note 14.pdf",
+    specialTariffTreatmentCodes: ["L"],
   },
   {
     title: "General Note 15",
     description: "Exclusions from tariff rate quotas",
     pdfURL: "/notes/general/General Note 15.pdf",
+    specialTariffTreatmentCodes: [],
   },
   {
     title: "General Note 16",
     description: "African Growth and Opportunity Act (AGOA)",
     pdfURL: "/notes/general/General Note 16.pdf",
+    specialTariffTreatmentCodes: ["D"],
   },
   {
     title: "General Note 17",
-    description: "African Growth and Opportunity Act (AGOA)",
+    description:
+      "United States-Caribbean Basin Trade Partnership Act of 2000 (CBPTA)",
     pdfURL: "/notes/general/General Note 17.pdf",
+    specialTariffTreatmentCodes: ["R"],
   },
   {
     title: "General Note 18",
     description: "United States-Jordan Free Trade Area Implementation Act",
     pdfURL: "/notes/general/General Note 18.pdf",
+    specialTariffTreatmentCodes: ["JO"],
   },
   {
     title: "General Note 25",
     description: "United States-Singapore Free Trade Agreement",
     pdfURL: "/notes/general/General Note 25.pdf",
+    specialTariffTreatmentCodes: ["SG"],
   },
   {
     title: "General Note 26",
     description: "United States-Chile Free Trade Agreement",
     pdfURL: "/notes/general/General Note 26.pdf",
+    specialTariffTreatmentCodes: ["CL"],
   },
   {
     title: "General Note 27",
     description: "United States-Morocco Free Trade Agreement",
     pdfURL: "/notes/general/General Note 27.pdf",
+    specialTariffTreatmentCodes: ["MA"],
   },
   {
     title: "General Note 28",
     description: "United States-Australia Free Trade Agreement",
     pdfURL: "/notes/general/General Note 28.pdf",
+    specialTariffTreatmentCodes: ["AU"],
   },
   {
     title: "General Note 29",
     description:
       "Dominican Republic-Central America-United States Free Trade Agreement (DR-CAFTA)",
     pdfURL: "/notes/general/General Note 29.pdf",
+    specialTariffTreatmentCodes: ["P", "P+"],
   },
   {
     title: "General Note 30",
     description: "United States-Bahrain Free Trade Agreement",
     pdfURL: "/notes/general/General Note 30.pdf",
+    specialTariffTreatmentCodes: ["BH"],
   },
   {
     title: "General Note 31",
     description: "United States-Oman Free Trade Agreement",
     pdfURL: "/notes/general/General Note 31.pdf",
+    specialTariffTreatmentCodes: ["OM"],
   },
   {
     title: "General Note 32",
     description: "United States-Peru Trade Promotion Agreement",
     pdfURL: "/notes/general/General Note 32.pdf",
+    specialTariffTreatmentCodes: ["PE"],
   },
   {
     title: "General Note 33",
     description: "United States-Korea Free Trade Agreement",
     pdfURL: "/notes/general/General Note 33.pdf",
+    specialTariffTreatmentCodes: ["KR"],
   },
   {
     title: "General Note 34",
     description: "United States-Colombia Trade Promotion Agreement",
     pdfURL: "/notes/general/General Note 34.pdf",
+    specialTariffTreatmentCodes: ["CO"],
   },
   {
     title: "General Note 35",
     description: "United States-Panama Trade Promotion Agreement",
     pdfURL: "/notes/general/General Note 35.pdf",
+    specialTariffTreatmentCodes: ["PA"],
   },
   {
     title: "General Note 36",
     description: "Trade Agreement between the United States and Japan",
     pdfURL: "/notes/general/General Note 36.pdf",
+    specialTariffTreatmentCodes: ["JP"],
   },
   {
     title: "General Statistical Notes",
     description: "General Statistical Notes",
     pdfURL: "/notes/general/General Statistical Notes.pdf",
+    specialTariffTreatmentCodes: [],
   },
 ];
