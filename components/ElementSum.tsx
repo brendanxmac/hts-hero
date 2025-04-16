656f8017c5fd58bc48f152cd9e7a69f1d9105c07
Brendan McLaughlin <brendanmclaughlin@Brendans-MacBook-Pro.local>
2025-04-16 19:10:33 +1000

    feat: ability to add and remove headings as candidates manually

--- components/ElementSum.tsx
+++ components/ElementSum.tsx
@@ -1,11 +1,11 @@
-import { ChevronRightIcon } from "@heroicons/react/16/solid";
+import { ChevronRightIcon, XMarkIcon } from "@heroicons/react/16/solid";
 import { HtsElement } from "../interfaces/hts";
 import { SecondaryInformation } from "./SecondaryInformation";
 import { NavigatableElement } from "./Elements";
 import SquareIconButton from "./SqaureIconButton";
 import { PlusIcon } from "@heroicons/react/24/solid";
 import { TertiaryInformation } from "./TertiaryInformation";
-
+import { useClassification } from "../contexts/ClassificationContext";
 interface Props {
   element: HtsElement;
   chapter: number;
@@ -19,40 +19,78 @@ export const ElementSum = ({
   breadcrumbs,
   setBreadcrumbs,
 }: Props) => {
-  const { htsno, description } = element;
+  const { htsno, description, indent } = element;
+  const { classificationProgression, setClassificationProgression } =
+    useClassification();
+
+  const isSelected = Boolean(
+    classificationProgression[0].candidates.find(
+      (candidate) => candidate.uuid === element.uuid
+    )
+  );
 
   return (
-    <div
-      onClick={(e) => {
-        e.stopPropagation();
-        setBreadcrumbs([
-          ...breadcrumbs,
-          {
-            title: `${htsno || description.split(" ").slice(0, 2).join(" ") + "..."}`,
-            element: {
-              ...element,
-              chapter,
-            },
-          },
-        ]);
-      }}
-      className="flex flex-col gap-2 w-full rounded-md bg-primary/30 dark:bg-primary/30 transition duration-100 ease-in-out cursor-pointer"
-    >
+    <div className="flex flex-col gap-2 w-full rounded-md bg-primary/30 dark:bg-primary/30 transition duration-100 ease-in-out cursor-pointer">
       <div className="flex">
-        <div className="flex items-center justify-center">
-          <div className="px-3">
-            <SquareIconButton
-              icon={<PlusIcon className="h-4 w-4" />}
-              onClick={() => {
-                // TODO: add heading to candidates via classification progression
-              }}
-            />
+        {indent === "0" && (
+          <div className="flex items-center justify-center">
+            <div className="px-3">
+              {isSelected ? (
+                <SquareIconButton
+                  icon={<XMarkIcon className="h-4 w-4" />}
+                  onClick={() => {
+                    // TODO: make a function for removing an element from a single classificatino progression
+                    const newClassificationProgression =
+                      classificationProgression.slice(0, 1);
+                    newClassificationProgression[0].candidates =
+                      newClassificationProgression[0].candidates.filter(
+                        (candidate) => candidate.uuid !== element.uuid
+                      );
+                    setClassificationProgression(newClassificationProgression);
+                  }}
+                  color="error"
+                />
+              ) : (
+                <SquareIconButton
+                  icon={<PlusIcon className="h-4 w-4" />}
+                  onClick={() => {
+                    // TODO: make a function for adding an element to a single classificatino progression
+                    const newClassificationProgression =
+                      classificationProgression.slice(
+                        0,
+                        classificationProgression.length
+                      );
+                    newClassificationProgression[0].candidates = [
+                      ...newClassificationProgression[0].candidates,
+                      element,
+                    ];
+
+                    setClassificationProgression(newClassificationProgression);
+                  }}
+                />
+              )}
+            </div>
+
+            <div className="h-full w-[1px] bg-base-300 dark:bg-base-content/10" />
           </div>
+        )}
 
-          <div className="h-full w-[1px] bg-base-300 dark:bg-base-content/10" />
-        </div>
-
-        <div className="flex items-center justify-between w-full hover:bg-primary/50 rounded-r-md">
+        <div
+          className="flex items-center justify-between w-full hover:bg-primary/50 rounded-r-md"
+          onClick={(e) => {
+            e.stopPropagation();
+            setBreadcrumbs([
+              ...breadcrumbs,
+              {
+                title: `${htsno || description.split(" ").slice(0, 2).join(" ") + "..."}`,
+                element: {
+                  ...element,
+                  chapter,
+                },
+              },
+            ]);
+          }}
+        >
           <div className="w-full flex flex-col items-start justify-between gap-1 p-4">
             {htsno && (
               <div className="min-w-20 md:min-w-32">
