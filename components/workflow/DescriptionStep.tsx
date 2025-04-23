4a48e34a22dfe65db7e2f2d8ed2a1e2272dac511
Brendan McLaughlin <brendanmclaughlin@Brendans-MacBook-Pro.local>
2025-04-24 09:13:11 +1000

    fix: width of workflow step content & navigation as well as inserting horizontal line to separate the two without needing to wrap in extra div

--- components/workflow/DescriptionStep.tsx
+++ components/workflow/DescriptionStep.tsx
@@ -27,29 +27,31 @@ export const DescriptionStep = ({
 
   return (
     <div className="h-full flex flex-col">
-      <div className="grow flex flex-col gap-10 px-8 justify-center border-b-2 border-base-100">
-        <div className="w-full max-w-3xl mx-auto">
-          <div className="flex flex-col gap-2">
-            <TertiaryText value="Step 1" color={Color.NEUTRAL_CONTENT} />
-            <PrimaryLabel
-              value="Describe the article you are classifying"
-              color={Color.WHITE}
-            />
-          </div>
-          <TextInput
-            placeholder="Enter item description"
-            defaultValue={productDescription}
-            onSubmit={(value) => {
-              setProductDescription(value);
-              setWorkflowStep(WorkflowStep.ANALYSIS);
-            }}
-            onChange={(value) => {
-              setLocalProductDescription(value);
-            }}
+      {/* Content */}
+      <div className="grow w-full max-w-3xl mx-auto flex flex-col px-8 justify-center gap-8">
+        <div className="flex flex-col gap-2">
+          <TertiaryText value="Step 1" color={Color.NEUTRAL_CONTENT} />
+          <PrimaryLabel
+            value="Describe the article you are classifying"
+            color={Color.WHITE}
           />
         </div>
+        <TextInput
+          placeholder="Enter item description"
+          defaultValue={productDescription}
+          onSubmit={(value) => {
+            setProductDescription(value);
+            setWorkflowStep(WorkflowStep.ANALYSIS);
+          }}
+          onChange={(value) => {
+            setLocalProductDescription(value);
+          }}
+        />
       </div>
-      <div className="w-full max-w-3xl mx-auto">
+      {/* Horizontal line */}
+      <div className="w-full border-t-2 border-base-100" />
+      {/* Navigation */}
+      <div className="w-full max-w-3xl mx-auto px-8">
         <StepNavigation
           next={{
             label: "Continue",
