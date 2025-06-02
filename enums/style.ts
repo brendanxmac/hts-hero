5f4ca0283455cba05abdead6c9bbbc8595677771
Brendan McLaughlin <brendanmclaughlin@Brendans-MacBook-Pro.local>
2025-06-02 16:55:38 +0700

    chore: Attempt to get the best candidate at each level of the HTS classification, automatically

--- enums/style.ts
+++ enums/style.ts
@@ -2,6 +2,7 @@ export enum Color {
   PRIMARY = "primary",
   SECONDARY = "secondary",
   TERTIARY = "tertiary",
+  ACCENT = "accent",
   BASE_CONTENT = "base-content",
   NEUTRAL_CONTENT = "neutral-content",
   WHITE = "white",
