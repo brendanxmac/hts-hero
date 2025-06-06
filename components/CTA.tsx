54d031011b1e26f20ace7a61d74fffbf067299fb
Brendan McLaughlin <brendanmclaughlin@Brendans-MacBook-Pro.local>
2025-06-06 09:53:40 +0700

    chore: copy updates on the importers page
    Former-commit-id: cb344dfb9d1fc97dcbab01476eba7e1a06f3d738

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -3,16 +3,14 @@ import Link from "next/link";
 
 const CTA = () => {
   return (
-    <section className="bg-neutral-900 hero overflow-hidden min-h-[75vh]">
-      <div className="relative hero-overlay bg-neutral-900"></div>
+    <section className="bg-base-100 hero overflow-hidden min-h-[75vh]">
+      <div className="relative hero-overlay bg-base-100"></div>
       <div className="relative hero-content text-center text-neutral-content p-8">
         <div className="flex flex-col items-center max-w-3xl p-8 md:p-0">
-          <h2 className="text-white font-bold text-4xl md:text-6xl tracking-tight mb-8 md:mb-12">
-            Get your HTS Code now!
+          <h2 className="text-white font-bold text-4xl md:text-5xl tracking-tight mb-8 md:mb-12">
+            Spare your sanity and your wallet
           </h2>
-          <p className="md:text-lg mb-12 md:mb-16">
-            Spare your sanity and your wallet, you deserve it.
-          </p>
+          <p className="md:text-lg mb-12 md:mb-16">Get your HTS Code now!</p>
 
           <Link
             className="btn btn-primary btn-wide"
