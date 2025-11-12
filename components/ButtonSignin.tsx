89526f66057adf00bca07c8f484bc4da38ec2f25
Brendan McLaughlin <brendanmclaughlin@Brendans-MacBook-Pro.local>
2025-11-12 12:04:13 +0700

    feat: email and password form, OTP implementation

--- components/ButtonSignin.tsx
+++ components/ButtonSignin.tsx
@@ -43,25 +43,7 @@ const ButtonSignin = ({ text, extraStyle }: Props) => {
       href={getRedirectUrl()}
       className={`btn btn-primary btn-sm ${extraStyle ? extraStyle : ""}`}
     >
-      {/* <div className="flex gap-2 items-center"> */}
-      {/* {pathname !== "/app" && <p className="text-lg">⚡️</p>} */}
       <p>{getButtonText()}</p>
-      {/* </div> */}
-      {/* {user?.user_metadata?.avatar_url ? (
-          <img
-            src={user?.user_metadata?.avatar_url}
-            alt={user?.user_metadata?.name || "Account"}
-            className="w-6 h-6 rounded-full shrink-0"
-            referrerPolicy="no-referrer"
-            width={24}
-            height={24}
-          />
-        ) : (
-          <span className="w-6 h-6 bg-base-300 flex justify-center items-center rounded-full shrink-0">
-            {user?.user_metadata?.name?.charAt(0) || user?.email?.charAt(0)}
-          </span>
-        )}
-        {user?.user_metadata?.name || user?.email || "Account"} */}
     </Link>
   );
 
