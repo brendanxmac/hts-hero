6c53755b6b62cab4e6eb647e53c3b8a7c53c28f7
Brendan McLaughlin <brendanmclaughlin@Brendans-MacBook-Pro.local>
2025-11-21 22:06:06 +0700

    feat: brazil tariff details & copy tariff updates

--- components/InlineCountryTariff.tsx
+++ components/InlineCountryTariff.tsx
@@ -115,7 +115,7 @@ export const InlineCountryTariff = ({
         const reviewText = tariff.value === null ? "Needs Review" : "";
         const valueText =
           tariff.type === "percent" ? `${tariff.value}%` : tariff.raw;
-        return `${tariffElement.htsno} - ${reviewText ? "Needs Review" : valueText} - General Duty (${primaryText})`;
+        return `   ${reviewText ? "Needs Review" : valueText} - General Duty (${primaryText}) - ${tariffElement.htsno}`;
       });
       return baseTariffsAsText.join("\n");
     }
@@ -125,12 +125,12 @@ export const InlineCountryTariff = ({
 
   const getTariffSetText = (tariffSet: TariffSet) => {
     const tariffSetTitle = tariffSet.name
-      ? `${tariffSet.name} Tariff Details`
-      : "Tariff Details";
+      ? `${tariffSet.name} Tariffs:`
+      : "Tariffs:";
     const baseTariffsText = getBaseTariffsText();
     const tariffSetText = tariffSet.tariffs.map((tariff) => {
       if (tariff.isActive) {
-        return `${tariff.code} - ${tariff[tariffColumn] === null ? "Needs Review" : `${tariff[tariffColumn]}% - ${tariff.name}`}`;
+        return `   ${tariff[tariffColumn] === null ? "Needs Review" : `${tariff[tariffColumn]}% - ${tariff.name} - ${tariff.code} `}`;
       }
       return "";
     });
@@ -182,8 +182,9 @@ export const InlineCountryTariff = ({
             return t.programs?.includes(selectedSpecialProgram.symbol);
           }
           return true;
-        })
-    )} %`;
+        }),
+      true
+    )}%`;
 
     const setTotal = is15PercentCapCountryLessThan15Percent
       ? cappedCountryRate
@@ -191,14 +192,17 @@ export const InlineCountryTariff = ({
         ? baseTariffRates + adValoremRate
         : adValoremRate;
 
-    return `${tariffSetTitle}\n${baseTariffsText}\n${tariffSetText.filter((t) => t !== "").join("\n")}\nTotal: ${setTotal}\n`;
+    return `${tariffSetTitle}\n${baseTariffsText}\n${tariffSetText.filter((t) => t !== "").join("\n")}\n\nTotal: ${setTotal}\n`;
   };
 
   const copyTariffDetails = () => {
+    const tariffContext = `Tariff Details for ${country.name}:`;
+    const harborMaintenanceFee = `Harbor Maintenance Fee:\n   0.125% of the value of the imported cargo`;
+    const merchandiseProcessingFee = `Merchandise Processing Fee:\n   0.3464% of the value of the imported cargo\n     Minimum: $33.58 (shipments valued at or below $9,640.00)\n     Maximum: $651.50 (shipments valued at or above $188,077.37)\n     Entries valued at or below $9,640.00 are exempt from the fee\n     Note: Informal entires under $2,500 face a flat fee of $2.69`;
     const tradeProgramText = selectedTradeProgram?.name
       ? `Trade Program: ${selectedTradeProgram.name}\n`
       : "";
-    return `${tradeProgramText}\n${tariffSets.map((set) => getTariffSetText(set)).join("\n")}`;
+    return `${tariffContext}\n\n${harborMaintenanceFee}\n\n${merchandiseProcessingFee}\n\n${tradeProgramText}${tariffSets.map((set) => getTariffSetText(set)).join("\n")}`;
   };
 
   useEffect(() => {
@@ -392,6 +396,7 @@ export const InlineCountryTariff = ({
               Note: {getNoteForConditionalReciprocalExemption()}{" "}
               <span className="underline">
                 is/are EXEMPT from reciprocal tariffs
+                {country.code === "BR" ? " and the Brazil 40% IEEPA" : ""}
               </span>
             </p>
             <p className="text-base-content">
